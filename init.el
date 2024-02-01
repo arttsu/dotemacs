affbc10f1e051fec7000e13d9a471a30f56455e9
Artem Tseranu <git@arttsu.com>
2024-02-01 23:38:04 +0100

    Add EMMS

--- init.el
+++ init.el
@@ -665,3 +665,16 @@ IGNORE ATTACHMENTS.")
   :bind
   (("C-c f" . link-hint-open-link)
    ("C-c y" . link-hint-copy-link)))
+
+(use-package emms
+  :defer t
+  :custom
+  (emms-player-list '(emms-player-mpv))
+  (emms-player-mpv-update-metadata t)
+  (emms-streams-file (expand-file-name "streams.emms" user-emacs-directory))
+  :config
+  (emms-all)
+  :bind
+  (("C-c r r" . 'emms-streams)
+   ("C-c r p" . 'emms-pause)
+   ("C-c r s" . 'emms-stop)))
