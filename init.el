59f3fe01c330b792f887121c31e51c9b6e3398f9
Artem Tseranu <git@arttsu.com>
2024-02-18 17:22:04 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,28 @@
+(setq straight-fix-flycheck t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+	"straight/repos/straight.el/bootstrap.el"
+	(or (bound-and-true-p straight-base-dir)
+	    user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+
+(use-package emacs
+  :custom
+  (create-lockfiles nil)
+  (make-backup-files nil))
+
+(use-package magit
+  :bind
+  (("C-c g" . magit-file-dispatch)))
