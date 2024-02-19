cf2a09f4d00f973c77e0dbc1e0f951a006fe0e45
Artem Tseranu <git@arttsu.com>
2024-02-19 14:26:53 +0100

    Add crux

--- init.el
+++ init.el
@@ -150,3 +150,10 @@
   (whole-line-or-region-global-mode)
   :bind
   (("M-/" . whole-line-or-region-comment-dwim)))
+
+(use-package crux
+  :bind
+  (("C-o" . crux-smart-open-line)
+   ("C-S-o" . crux-smart-open-line-above)
+   ("C-^" . crux-top-join-line)
+   ("C-M-; D" . crux-duplicate-and-comment-current-line-or-region)))
