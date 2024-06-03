970cf46fc5b6deb2abee701a3019662c9bd22adb
Artem Tseranu <git@arttsu.com>
2024-06-04 00:00:02 +0200

    Add Go to Char

--- init.el
+++ init.el
@@ -220,6 +220,15 @@
   :bind
   (("C-=" . er/expand-region)))
 
+(use-package iy-go-to-char
+  :bind
+  (("M-g f" . iy-go-to-char)
+   ("M-g F" . iy-go-to-char-backward)
+   ("M-g t" . iy-go-up-to-char)
+   ("M-g T" . iy-go-up-to-char-backward)
+   ("M-g ;" . iy-go-to-or-up-to-continue)
+   ("M-g ," . iy-go-to-or-up-to-continue-backward)))
+
 (use-package magit
   :bind
   (("C-c g" . magit-file-dispatch)))
