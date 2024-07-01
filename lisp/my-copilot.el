b3b774b925ac7861b32ebbd5acc9589b44789e1d
Artem Tseranu <artem.tseranu@xing.com>
2024-07-01 18:07:05 +0200

    Fix conflicting keybindings - Copilot and tab bar

--- lisp/my-copilot.el
+++ lisp/my-copilot.el
@@ -1,8 +1,8 @@
 (straight-use-package 'hydra)
 
-(defhydra my-copilot-accept-completion (copilot-mode-map "C-<tab>")
+(defhydra my-copilot-accept-completion (copilot-mode-map "C-M-<tab>")
   "Accept Copilot completion"
-  ("C-<tab>" copilot-accept-completion "Accept" :color blue)
+  ("C-M-<tab>" copilot-accept-completion "Accept" :color blue)
   ("M-f" copilot-accept-completion-by-word "By word")
   ("C-e" copilot-accept-completion-by-line "By line"))
 
