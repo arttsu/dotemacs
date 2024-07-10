af23b423929b6ccce20ac076c9de7aa16bd8a73d
Artem Tseranu <artem.tseranu@xing.com>
2024-07-10 18:06:09 +0200

    Implement my split window sensibly

--- lisp/my-emacs.el
+++ lisp/my-emacs.el
@@ -11,4 +11,16 @@
   "<left>" #'tab-bar-history-back
   "<right>" #'tab-bar-history-forward)
 
+(defun my-split-window-sensibly (&optional window)
+    (interactive)
+    (let ((window (or window (selected-window))))
+        (or (and (window-splittable-p window t)
+                 (with-selected-window window
+                     (split-window-right)))
+            (and (window-splittable-p window)
+                 (with-selected-window window
+                     (split-window-below))))))
+
+(setq split-window-preferred-function #'my-split-window-sensibly)
+
 (provide 'my-emacs)
