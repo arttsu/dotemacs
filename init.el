9665455906658596093d98d14f9469ff73c54409
Artem Tseranu <artem.tseranu@xing.com>
2025-07-01 17:43:46 +0200

    add modus themes

--- init.el
+++ init.el
@@ -80,18 +80,18 @@
   (initial-scratch-message "✅ All systems go! 🚀🪐✨")
   (initial-major-mode 'text-mode)
   (visible-bell t)
-  
+
   ;; Editing behavior
   (indent-tabs-mode nil)
   (tab-width 4)
   (require-final-newline t)
   (save-interprogram-paste-before-kill t)
-  
+
   ;; File handling
   (create-lockfiles nil)
   (make-backup-files nil)
   (global-auto-revert-non-file-buffers t)
-  
+
   ;; Security and permissions
   (epg-pinentry-mode 'loopback)
   (disabled-command-function nil)
@@ -107,7 +107,7 @@
   ;; Font configuration
   (set-face-attribute 'default nil :font my-font :height my-font-height)
   (set-frame-font my-font nil t)
-  
+
   ;; Better emoji support on Windows
   (when (my-windows-p)
     (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'append))
@@ -119,12 +119,33 @@
   (tab-bar-mode)
   (tab-bar-history-mode)
   (repeat-mode)
-  
+
   ;; Simplify prompts
   (fset 'yes-or-no-p 'y-or-n-p)
 
   ;; Only in programming buffers to avoid clutter in text modes
-  (add-hook 'prog-mode-hook 'display-line-numbers-mode))
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+  :bind
+  (;; Navigation
+   ("C-c j x" . scratch-buffer)
+   ("C-c j h" . my-jump-home)
+   ("<f8>" . my-pop-mark)
+   ("C-M-<return>" . tab-switch)
+
+   ;; Text navigation
+   ("M-g w" . forward-to-word)
+   ("M-g W" . backward-to-word)
+
+   ;; Editing
+   ("C-M-; d" . duplicate-dwim)
+   ("C-c d h" . erase-buffer)
+   ("M-z" . zap-up-to-char)
+   ("M-Z" . zap-to-char)
+
+   ;; Mouse
+   ([down-mouse-2] . mouse-set-point)
+   ([mouse-2] . delete-window)))
 
 (defun my-pop-mark ()
   "Jump back to previous mark position"
@@ -145,24 +166,17 @@
   (dired-listing-switches (cond ((my-windows-p) dired-listing-switches)
                                 (t "-alh --group-directories-first"))))
 
-(use-package emacs
-  :bind
-  (;; Navigation
-   ("C-c j x" . scratch-buffer)
-   ("C-c j h" . my-jump-home)
-   ("<f8>" . my-pop-mark)
-   ("C-M-<return>" . tab-switch)
-   
-   ;; Text navigation
-   ("M-g w" . forward-to-word)
-   ("M-g W" . backward-to-word)
-   
-   ;; Editing
-   ("C-M-; d" . duplicate-dwim)
-   ("C-c d h" . erase-buffer)
-   ("M-z" . zap-up-to-char)
-   ("M-Z" . zap-to-char)
-   
-   ;; Mouse
-   ([down-mouse-2] . mouse-set-point)
-   ([mouse-2] . delete-window)))
+(use-package modus-themes
+  :ensure
+  :custom
+  (modus-themes-italic-constructs t)
+  (modus-themes-org-blocks 'gray-background)
+  (modus-themes-headings '((0 . (ultrabold 1.9))
+                           (1 . (ultrabold 1.7))
+                           (2 . (extrabold 1.5))
+                           (3 . (extrabold 1.3))
+                           (4 . (extrabold 1.1))
+                           (t . (extrabold))))
+  (modus-themes-variable-pitch-ui t)
+  :config
+  (modus-themes-load-theme 'modus-vivendi))
