f89cbd4a764592ae54d8416e2594392c024a424f
Artem Tseranu <artem.tseranu@xing.com>
2025-07-01 17:57:29 +0200

    add Corfu

--- init.el
+++ init.el
@@ -198,3 +198,20 @@
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package corfu
+  :ensure
+  :demand
+  :custom
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 2)
+  (corfu-quit-at-boundary 'separator)
+  (corfu-quit-no-match t)
+  :config
+  (global-corfu-mode)
+  (corfu-history-mode)
+  :bind
+  (:map corfu-map
+        ("C-SPC" . corfu-insert-separator)
+        ("C-;" . corfu-quick-insert)))
