572a2bb04efe436ef27315add53dc5c5821a856e
Artem Tseranu <git@arttsu.com>
2025-09-30 23:12:50 +0200

    Move font settings to local init

--- local-init-example.el
+++ local-init-example.el
@@ -1,2 +1,8 @@
-(setq my-font "Cascadia Mono")
-(setq my-font-height 90)
+;; Built-in font on MacOS - Menlo.
+;; Built-in font on Windows - Cascadia Code.
+;; (let ((font "Liberation Mono"))
+;;   (set-face-attribute 'default nil :font font :height 100)
+;;   (set-frame-font font nil t))
+
+;; Better emoji support on Windows.
+;; (set-fontset-font t 'unicode "Segoe UI Emoji" nil 'append)
