5161c0c6e9fff18e80c0b7b468948dd2431698f0
Artem Tseranu <git@arttsu.com>
2025-12-21 17:58:57 +0100

    Add Smartparens

--- init.el
+++ init.el
@@ -56,3 +56,32 @@
   :bind
   (:map global-map
         ("C-c g" . magit-file-dispatch)))
+
+;;; Smartparens
+;; https://github.com/Fuco1/smartparens
+
+(use-package smartparens
+  :ensure
+  :custom
+  (sp-highlight-pair-overlay nil)
+  :config
+  (require 'smartparens-config)
+  :hook
+  (prog-mode . smartparens-mode)
+  :bind
+  (:map smartparens-mode-map
+        ("C-<right>" . sp-forward-slurp-sexp)
+        ("C-<left>" . sp-backward-slurp-sexp)
+        ("M-<right>" . sp-forward-barf-sexp)
+        ("M-<left>" . sp-backward-barf-sexp)
+        ("M-a" . sp-beginning-of-sexp)
+        ("M-e" . sp-end-of-sexp)
+        ("C-M-u" . sp-up-sexp)
+        ("C-M-S-u" . sp-backward-up-sexp)
+        ("C-M-d" . sp-down-sexp)
+        ("C-M-S-d" . sp-backward-down-sexp)
+        ("C-c p u" . sp-unwrap-sexp)
+        ("C-c p (" . sp-wrap-round)
+        ("C-c p [" . sp-wrap-square)
+        ("C-c p {" . sp-wrap-curly)
+        ("C-c p r" . sp-rewrap-sexp)))
