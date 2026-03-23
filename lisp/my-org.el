c814143df3f8e59a3553cf3ea078f85fb9819dea
Artem Tseranu <git@arttsu.com>
2026-03-23 15:07:42 +0100

    Fix interstitial journal entry time

--- lisp/my-org.el
+++ lisp/my-org.el
@@ -39,7 +39,7 @@ PLAN should be either \"A\" or \"B\"."
     ("ga" "plan A session" entry ,(my-org-gym-log-target "A") (file ,(my-org-template "gym/plan-a")))
     ("gb" "plan B session" entry ,(my-org-gym-log-target "B") (file ,(my-org-template "gym/plan-b")))
     ("j" "Journal")
-    ("jj" "local entry" entry (file+olp+datetree ,(expand-file-name "local/journals/current-journal.org" my-org-dir)) ,(format "**** %s\n%%?" (format-time-string "%H:%M")))))
+    ("jj" "local entry" entry (file+olp+datetree ,(expand-file-name "local/journals/current-journal.org" my-org-dir)) (function (lambda () (format "**** %s\n%%?" (format-time-string "%H:%M")))))))
 
 (defun my-org-agenda-files (context)
   "Return the list of Org agenda files in the specified context.
