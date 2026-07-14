10272f616d8e93ed1adf473c42e46162d1c30aa4
Artem Tseranu <git@arttsu.com>
2026-07-14 09:48:48 +0200

    Improve ledger reports

--- init.el
+++ init.el
@@ -467,8 +467,8 @@
     (ledger-reports-add "bal-this-month" "%(binary) -f %(ledger-file) --invert --period 'this month' -S amount bal ^Income ^Expenses")
     (ledger-reports-add "bal-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' -S amount bal ^Income ^Expenses")
     (ledger-reports-add "bal-this-week" "%(binary) -f %(ledger-file) --invert --period 'this week' -S amount bal ^Income ^Expenses")
-    (ledger-reports-add "bal-last-week" "%(binary) -f %(ledger-file) --invert --period 'last week' -S amount bal ^Income ^Expenses")
-    (ledger-reports-add "fixed-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' bal Fixed")))
+    (ledger-reports-add "bal-last-week" "%(binary) -f %(ledger-file) --invert --period 'last week' -S amount --limit 'account !~ /:(MM|YY)$/' bal ^Income ^Expenses")
+    (ledger-reports-add "fixed-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' bal MM YY")))
 
 ;;; Org
 
