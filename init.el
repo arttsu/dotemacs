e6f47280c6720fafb15814ef11f7f04eb6232751
Artem Tseranu <git@arttsu.com>
2026-08-05 23:18:47 +0200

    Update ledger reports
    Support bi-monthly entries.

--- init.el
+++ init.el
@@ -467,8 +467,8 @@
     (ledger-reports-add "bal-this-month" "%(binary) -f %(ledger-file) --invert --period 'this month' -S amount bal ^Income ^Expenses")
     (ledger-reports-add "bal-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' -S amount bal ^Income ^Expenses")
     (ledger-reports-add "bal-this-week" "%(binary) -f %(ledger-file) --invert --period 'this week' -S amount bal ^Income ^Expenses")
-    (ledger-reports-add "bal-last-week" "%(binary) -f %(ledger-file) --invert --period 'last week' -S amount --limit 'account !~ /:(MM|YY)$/' bal ^Income ^Expenses")
-    (ledger-reports-add "fixed-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' bal MM YY")))
+    (ledger-reports-add "bal-last-week" "%(binary) -f %(ledger-file) --invert --period 'last week' -S amount --limit 'account !~ /:(MM|BM|YY)$/' bal ^Income ^Expenses")
+    (ledger-reports-add "fixed-last-month" "%(binary) -f %(ledger-file) --invert --period 'last month' bal MM BM YY")))
 
 ;;; Org
 
